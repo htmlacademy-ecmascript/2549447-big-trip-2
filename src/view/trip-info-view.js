57ad7e0f2439205442dbb30463ee99047265c6da
OlegSulitskii <sulitsky2011@yandex.ru>
2025-05-19 12:13:38 +0400

    - вносит правки по е2е #03

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -1,7 +1,7 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 function createTripInfoTemplate(formattedTripPath, formattedDurationOfTrip, fullCostOfTrip) {
-  return `${formattedTripPath || formattedDurationOfTrip || fullCostOfTrip ?
+  return `${fullCostOfTrip > 0 ?
     `<section class="trip-main__trip-info  trip-info">
       <div class="trip-info__main">
         <h1 class="trip-info__title">${ formattedTripPath }</h1>
